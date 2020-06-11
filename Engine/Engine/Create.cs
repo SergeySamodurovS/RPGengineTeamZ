53f9155c71e2485c83322decddb6e4400849de79
SergeySamodurovS <58858262+SergeySamodurovS@users.noreply.github.com>
2020-06-11 19:28:03 +0300

    Update Create.cs

--- Engine/Engine/Create.cs
+++ Engine/Engine/Create.cs
@@ -8,23 +8,22 @@ namespace Engine
     {
         public static Equipment MakingEquipment(EquipmentType type)
         {
-
             switch (type)
             {
                 case EquipmentType.Helmet:
-                    Equipment equipmentHelmet = new Equipment("Helmet", 10, 15, EquipmentType.Helmet);//Шлем
+                    Equipment equipmentHelmet = new Equipment("Helmet", 10, 15, EquipmentType.Helmet);      //Шлем
                     return equipmentHelmet;
                 case EquipmentType.Сuirass:
-                    Equipment equipmentСuirass = new Equipment("Сuirass", 15, 20, EquipmentType.Сuirass);//Кираса
+                    Equipment equipmentСuirass = new Equipment("Сuirass", 15, 20, EquipmentType.Сuirass);   //Кираса
                     return equipmentСuirass;
                 case EquipmentType.Belt:
-                    Equipment equipmentBelt = new Equipment("Belt", 5, 7, EquipmentType.Belt);//Пояс
+                    Equipment equipmentBelt = new Equipment("Belt", 5, 7, EquipmentType.Belt);              //Пояс
                     return equipmentBelt;
                 case EquipmentType.Leggings:
-                    Equipment equipmentLeggings = new Equipment("Leggings", 8, 10, EquipmentType.Leggings);//Поножи
+                    Equipment equipmentLeggings = new Equipment("Leggings", 8, 10, EquipmentType.Leggings); //Поножи
                     return equipmentLeggings;
                 case EquipmentType.Gloves:
-                    Equipment equipmentGloves = new Equipment("Gloves", 8, 10, EquipmentType.Gloves);//Перчатки
+                    Equipment equipmentGloves = new Equipment("Gloves", 8, 10, EquipmentType.Gloves);       //Перчатки
                     return equipmentGloves;
                 case EquipmentType.Null:
                     Equipment equipmentNull = new Equipment("Null", 0, 0, EquipmentType.Null);
@@ -39,19 +38,19 @@ namespace Engine
             switch (type)
             {
                 case WeaponType.Bow:
-                    Weapon weaponBow = new Weapon("Bow", 10, 10, WeaponType.Bow);//лук
+                    Weapon weaponBow = new Weapon("Bow", 10, 10, WeaponType.Bow);       //лук
                     return weaponBow;
                 case WeaponType.Dirk:
-                    Weapon weaponDirk = new Weapon("Dirk", 5, 5, WeaponType.Dirk);//кинжал
+                    Weapon weaponDirk = new Weapon("Dirk", 5, 5, WeaponType.Dirk);      //кинжал
                     return weaponDirk;
                 case WeaponType.Fists:
-                    Weapon weaponFists = new Weapon("Fists", 2, 2, WeaponType.Fists);//костет
+                    Weapon weaponFists = new Weapon("Fists", 2, 2, WeaponType.Fists);   //костет
                     return weaponFists;
                 case WeaponType.Stick:
-                    Weapon weaponStick = new Weapon("Stick",8, 8, WeaponType.Stick);//посох
+                    Weapon weaponStick = new Weapon("Stick",8, 8, WeaponType.Stick);    //посох
                     return weaponStick;
                 case WeaponType.Sword:
-                    Weapon weaponSword = new Weapon("Sword", 12, 12, WeaponType.Sword);//меч
+                    Weapon weaponSword = new Weapon("Sword", 12, 12, WeaponType.Sword); //меч
                     return weaponSword;
                 case WeaponType.Null:
                     Weapon weaponNull = new Weapon("Null", 0, 0, WeaponType.Null);
