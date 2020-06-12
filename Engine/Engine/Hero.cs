beda7e67b8176f3f5c6a96404c9b5ba7586bcd6b
Aleksey Merkulov <mnement1988@yandex.ru>
2020-06-12 14:16:08 +0300

    Change Hero class

--- Engine/Engine/Hero.cs
+++ Engine/Engine/Hero.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Engine
 {
+    public delegate void Message(string msg);
     public class Hero
     {
+        public event Message Info;
         public string Name { private set; get; } //Имя
         public int Level { private set; get; } //Уровень
         public int Experience { private set; get; } //Опыт
         public int Damage { private set; get; } //Урон
         public int Balance { private set; get; } //Монеты
-        public Equipment Equipment { private set; get; } //Экипировка
         public Weapon Weapon { private set; get; } //Оружие
         public int Health { private set; get; } //Здоровье
         public int Mana { private set; get; } //Мана
         public Magic Magic { private set; get; } //Магия
+        public bool IsAlive { set; get; } //Жив ещё
+
+        public List<Equipment> equipment; //список экипировки
 
         public Hero(string name)
         {
@@ -24,6 +30,51 @@ namespace Engine
             Health = 100;
             Mana = 0;
             Balance = 10;
+            IsAlive = true;
+            equipment = new List<Equipment>();
+            Info($"Герой {this.Name} пришёл в этот мир {DateTime.Now}. У Вас есть {this.Health} единиц здоровья, {this.Mana} единиц маны, Вы наносите {this.Damage} единиц урона, Вам выдано {this.Balance} золотых.");
+        }
+        
+        public void BuyingEquipment(Equipment purchasedEquipment) 
+        {
+            if ((Balance >= purchasedEquipment.cost) && !equipment.Contains(purchasedEquipment))
+            {
+                Info?.Invoke($"Вы купили {purchasedEquipment.type} и потратили {purchasedEquipment.cost}");
+                equipment.Add(purchasedEquipment);
+                Balance -= purchasedEquipment.cost;
+            }
+            else if(Balance < purchasedEquipment.cost)
+            {
+                Info?.Invoke("Средств не достаточно");
+            }else if (equipment.Contains(purchasedEquipment))
+            {
+                Info?.Invoke($"У вас уже есть {purchasedEquipment.type}");
+            }
+        }
+        public void BuyingWeapons(Weapon purchasedWeapon)
+        {
+            if (Balance >= purchasedWeapon.cost)
+            {
+                Info?.Invoke("Покупка совершена");
+                Weapon = purchasedWeapon;
+                Balance -= purchasedWeapon.cost;
+            }
+            else
+            {
+                Info?.Invoke("Средств не достаточно");
+            }
+        }
+        public void AliveOrDead(int damage) 
+        {
+            if ((Health - damage) <= 0)
+            {
+                Info?.Invoke("Вы мертвы");
+                IsAlive = false;
+            }
+            else 
+            {
+                Info?.Invoke("Вы ещё живы, продолжайте");
+            }
         }
     }
 }
